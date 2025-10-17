c50e2334e7944faf6ac44a3369313149557a7691
tswt0825 <tswt0825@gmail.com>
2025-10-17 14:44:23 +0200

    start

---
+++ script.js
@@ -0,0 +1,44 @@
+const book = [
+  {
+    id: "Book-001",
+    title: "Eragon",
+    author: "Christopher Paolini",
+    genre: "Fantasy",
+    pages: 250,
+    favorite: false,
+  },
+  {
+    id: "Book-002",
+    title: "Den eldste",
+    author: "Christopher Paolini",
+    genre: "Fantasy",
+    pages: 250,
+    favorite: false,
+  },
+  {
+    id: "book-003",
+    title: "Brisingr",
+    author: "Christopher Paolini",
+    genre: "Fantasy",
+    pages: 250,
+    favorite: false,
+  },
+  {
+    id: "book-004",
+    title: "Arven",
+    author: "Christopher Paolini",
+    genre: "Fantasy",
+    pages: 250,
+    favorite: false,
+  },
+  {
+    id: "book-005",
+    title: "Dune",
+    author: "Frank Herbert",
+    genre: "Science Fiction",
+    pages: 412,
+    favorite: true,
+  },
+];
+
+console.log(book);
